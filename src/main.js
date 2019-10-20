a1e50ed5aece995eb373af70b3cc3b8b5bdab9de
changxianglin <281532612@qq.com>
2019-10-20 15:42:34 +0800

    write header component

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import fastclick from 'fastclick'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -6,6 +7,8 @@ import store from './store'
 Vue.config.productionTip = false
 
 import 'common/stylus/index.styl' 
+
+fastclick.attach(document.body)
 
 new Vue({
   router,

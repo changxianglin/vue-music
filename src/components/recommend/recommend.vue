55c999068f578299b0229c33dbd449c2ae402722
changxianglin <281532612@qq.com>
2019-10-20 17:13:36 +0800

    create slider component

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="recommend">
     <div class="recommend-content">
-      <div class="slider-wrapper">
-
+      <div v-if="recommends.length" class="slider-wrapper">
+        <slider>
+          <div v-for="(item, index) in recommends" v-bind:key="index">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl" alt="">
+            </a>
+          </div>
+        </slider>
       </div>
       <div class="recommend-list">
         <h1 class="recommend-title">热门歌单推荐</h1>
@@ -13,9 +19,16 @@
 </template>
 
 <script type="text/ecmascript-6">
+import Slider from 'base/slider/slider'
 import { getRecommend } from 'api/recommend'
 import { ERR_OK } from 'api/config'
+
 export default {
+  data() {
+    return {
+      recommends: []
+    }
+  },
   created() {
     this._getRecommend()
   },
@@ -23,10 +36,13 @@ export default {
     _getRecommend() {
       getRecommend().then((res) => {
         if(res.code === ERR_OK) {
-          console.log(res.data.slider)
+          this.recommends = res.data.slider
         }
       })
     }
+  },
+  components: {
+    Slider
   }
 }
 </script>

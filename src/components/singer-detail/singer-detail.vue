d39e0bff47c9e2ad24a24de38ebb27676d8ac009
changxianglin <281532612@qq.com>
2019-10-22 00:41:45 +0800

    singer detail page

---
+++ src/components/singer-detail/singer-detail.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="singer">
+    歌手像以前
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>

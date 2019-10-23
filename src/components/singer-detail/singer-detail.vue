3cc3a2fe4da6565df4630a0eaed122f4fcec024c
changxianglin <281532612@qq.com>
2019-10-23 20:13:33 +0800

    get singer data

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -39,6 +39,7 @@
         }
         getSingerDetail(this.singer.id).then((res) => {
           if (res.code === ERR_OK) {
+            console.log(res)
             this.songs = this._normalizeSongs(res.data.list)
           }
         })

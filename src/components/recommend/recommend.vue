525802154b49f5c8c758e5253397b168e8fe524f
changxianglin <281532612@qq.com>
2019-10-29 00:33:04 +0800

    use vuex and vue mixin

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -40,8 +40,10 @@
   import Scroll from 'base/scroll/scroll'
   import {getRecommend, getDiscList} from 'api/recommend'
   import {ERR_OK} from 'api/config'
+  import { playlistMixin } from 'common/js/mixin' 
 
   export default {
+    mixins: [playlistMixin],
     data() {
       return {
         recommends: [],
@@ -54,6 +56,11 @@
       this._getDiscList()
     },
     methods: {
+      handlePlaylist(playlist) {
+        const bottom = playlist.length > 0 ? '60px': ''
+        this.$refs.recommend.style.bottom = bottom
+        this.$refs.scroll.refresh()
+      },
       loadImage() {
         if (!this.checkloaded) {
           this.checkloaded = true

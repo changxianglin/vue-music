21cd4fe6d92daa33d2a7ce9018e9ff2c5b6f23c3
changxianglin <281532612@qq.com>
2019-12-03 23:57:50 +0800

    create switches component

---
+++ src/base/switches/switches.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="switches">
+    <ul class="switches">
+      <li class="switch-item">
+        <span></span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  .switches
+    display: flex
+    align-items: center
+    width: 240px
+    margin: 0 auto
+    border: 1px solid $color-highlight-background
+    border-radius: 5px
+    .switch-item
+      flex: 1
+      padding: 8px
+      text-align: center
+      font-size: $font-size-medium
+      color: $color-text-d
+      &.active
+        background: $color-highlight-background
+        color: $color-text
+</style>
